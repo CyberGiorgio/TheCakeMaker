39bbd3e6373f57e1d948dfbcdf766a7c4b694607
CyberGiorgio <giorgio.basile992@hotmail.com>
2022-04-26 12:25:07 +0100

    update 2/04/2022
    secure login logic

--- loginAccess.php
+++ loginAccess.php
@@ -1,50 +1,41 @@
 <?php
    include("config.php");
    session_start();
-   
-   try{
-         if(isset($_POST['login'])) {
-                                  // username and password sent from form 
-            if(isset($_POST['g-recaptcha-response'])){
-            $captcha=$_POST['g-recaptcha-response'];
-            }
-            if(!$captcha){
-                 $error = "Are you a robot?";
-            } else {
-            $myemail = mysqli_real_escape_string($db,$_POST['email']);
-            $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-                                       //query to collect info required
-            $sql = "SELECT * FROM user WHERE email = '$myemail' AND password = '$mypassword'";
-            $result = mysqli_query($db,$sql);
-            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-            $indexLevel = $row["level"];
-            $active = $row['active'];
-            $count = mysqli_num_rows($result);
-            
-            // If result matched $myusername and $mypassword, table row must be 1 row
-              
-            if($count == 1) {
-               //session_register("myusername");
-               $_SESSION['login_user'] = $myemail;
-             
-              // myStorage = window.sessionStorage;
-              // sessionStorage.setItem('login_user', $myemail);
-                           //redirected depending on the level
-               if($indexLevel == 1){
-               header("location: welcomeLevel1.php");
-              } else if ($indexLevel == 2){
-               header("location: welcomeLevel2.php");
-              } else if ($indexLevel == 3){
-               header("location: welcomeLevel3.php");
-              }
-            }else {           //login failed
-               $error = "Your Login Name or Password is invalid";
-            }
-
+   if(isset($_POST['login'])) {
+     if(isset($_POST['g-recaptcha-response'])){ //recaptcha done?
+         $captcha=$_POST['g-recaptcha-response'];
          }
-      }
-      }catch(Exception $e){
-         echo $e->getMessage();
-     }
+      if(!$captcha){       //recaptcha faild
+           $error = "Are you a robot?";
+      } else {
+         $email = mysqli_real_escape_string($db,$_POST['email']);
+         $password = mysqli_real_escape_string($db,$_POST['password']);
+         $level = mysqli_real_escape_string($db,$_POST['level']);
+         $stmt = $db->prepare("SELECT * FROM user WHERE email=? AND password=? LIMIT 1");
+         $stmt->bind_param('ss', $email, $password);     //secure prepare statement
+         $stmt->execute();
+         $stmt->bind_result($email, $password);
+         $stmt->store_result();
 
-   ?>
+         if($stmt->num_rows > 0)  //To check if the row exists
+            {
+               mysqli_stmt_execute($stmt);
+               $result = mysqli_stmt_get_result($stmt);
+               $row = mysqli_fetch_array($result, MYSQLI_NUM);
+               $indexLevel = $row[5];  //index 5 = level of user
+               $_SESSION['login_user'] = $email;   //store the session   
+               if($indexLevel == 1){      //redirect user
+                 header("Location: welcomeLevel1.php");
+               } else if ($indexLevel == 2){
+                 header("Location: welcomeLevel2.php");
+               } else if ($indexLevel == 3){
+                 header("Location: welcomeLevel3.php");
+            }
+         }else {           //login failed
+            $error = "  Your Login Name or Password is invalid $indexLevel";
+         }
+      $stmt->close();
+      $db->close();
+      }
+   }
+?>
